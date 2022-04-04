2ff331159639eaf6ef52ebc40468509d586bd49e
Tennyson Horn <thorn67@gmail.com>
2022-04-04 11:06:23 -0500

    solution to kata requiring to return true if int1 and int2 are not both even or odd.

--- loveMeNots.js
+++ loveMeNots.js
@@ -2,6 +2,7 @@
 //
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
+
 //solution
 function lovefunc(flower1, flower2){
   // moment of truth
