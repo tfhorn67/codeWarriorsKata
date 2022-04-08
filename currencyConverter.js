687330b311a0011d5e5a1403bc7d23f695781c4c
Tennyson Horn <thorn67@gmail.com>
2022-04-08 11:57:10 -0500

    solution to kata requiring to convert USD to CNY.

--- currencyConverter.js
+++ currencyConverter.js
@@ -5,6 +5,7 @@
 // * 465 -> '3138.75 Chinese Yuan'
 // The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
 
+
 //solution
 function usdcny(usd) {
   return `${(6.75 * usd).toFixed(2)} Chinese Yuan`;
