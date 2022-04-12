15becb17faf846706d1d4851192f6f4a237cffe1
Tennyson Horn <thorn67@gmail.com>
2022-04-12 12:14:53 -0500

    solution to kata requiring to count a certain number of sheep.

--- CountSheep.js
+++ CountSheep.js
@@ -3,6 +3,7 @@
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
+
 //solution
 var countSheep = function (num){
   //your code here
