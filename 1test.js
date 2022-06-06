16924a26961e78694f72c0de0c78e85c9a220739
Tennyson Horn <thorn67@gmail.com>
2022-06-06 07:17:58 -0500

    Solution to kata requiring to return the minimum number of bills required to give desired withdrawal amount at an ATM.

--- 1test.js
+++ 1test.js
@@ -1,18 +1,40 @@
-// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+// An ATM has banknotes of nominal values 10, 20, 50, 100, 200 and 500 dollars. You can consider that there is a large enough supply of each of these banknotes.
 //
-// Example:
+// You have to write the ATM's function that determines the minimal number of banknotes needed to honor a withdrawal of n dollars, with 1 <= n <= 1500.
 //
-// a=[1,2,0,3]
-// a.length-1=3
-// a includes [0,3] ,hence the function should return true
+// Return that number, or -1 if it is impossible.
+//
+// Good Luck!!!
+
 
 
 //solution
-function isAllPossibilities(x){
-  if (x.length === 0) {
-    return false
+function solve(n) {
+  let total = n
+  let billCount = 0
+  let billTypes = [500, 200, 100, 50, 20, 10]
+  let billPosition = 0
+  let dispensing = billTypes[billPosition]
+  //make sure exact change can be made
+  if (total % 10 !== 0) {
+    return -1
   }
-  return x.sort((a, b) => a - b).every((element, index) => {
-      return element === index
-  })
+  /*in a while loop, start counting bills and subtracting them from total
+    while total > 0, subtract largest bill that doesn't put total < 0,
+    if bill is larger than remaining total, decrement to next smallest bill and try again
+    increment bill count each time succeeds.*/
+  while (total > 0) {
+    if (total - dispensing >= 0) {
+      billCount++
+      total -= dispensing
+    } else {
+      billPosition++
+      dispensing = billTypes[billPosition]
+    }
+  }
+  return billCount
 }
+
+//short on time here but just a quick time saving measure would be to
+//increase bill count by total/dispensing and then reassign total to toal % dispensing
+//that way loop would only run once per bill type at most
