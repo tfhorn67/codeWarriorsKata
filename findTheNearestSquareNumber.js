01b18abe4b74daf48e5082de643abe5d150f7349
Tennyson Horn <thorn67@gmail.com>
2022-11-02 09:25:11 -0500

    solution to kata requiring to find the nearest perfect square to a given integer

---
+++ findTheNearestSquareNumber.js
@@ -0,0 +1,9 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+//
+// Goodluck :)
+
+
+//solution
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
