16ddaaeb8ec56ef4300bb6dfc89bc4a7598d8fe7
Tennyson Horn <thorn67@gmail.com>
2022-12-12 08:17:22 -0600

    solution to kata requiring to fail a bunch of tests in order to realize the author expected you to know and recognize the results of bitwise operations

---
+++ thinkingAndTestingAandB.js
@@ -0,0 +1,30 @@
+// No Story
+//
+// No Description
+//
+// Only by Thinking and Testing
+//
+// Look at result of testcase, guess the code!
+//
+
+
+
+//solution
+function testit(a,b){
+//   if(a == b){
+//     return a
+//   }
+//   if(b > a){
+//     return b
+//   }
+//    if (b > a && a === 1){
+//     return b+a
+//   }
+
+
+
+//   else{
+//     return a+b
+//   }
+  return a|b
+}
