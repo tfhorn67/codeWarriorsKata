154fa83f7ddb2936a55c96b3f7226880f0be39d6
Tennyson Horn <thorn67@gmail.com>
2022-12-11 18:53:04 -0600

    solution to kata requiring to figure out the result of a very strange ice breaker.

---
+++ strangePrincipal.js
@@ -0,0 +1,39 @@
+// A high school has a strange principal. On the first day, he has his students perform an odd opening day ceremony:
+//
+// There are number of lockers "n" and number of students "n" in the school. The principal asks the first student to go to every locker and open it. Then he has the second student go to every second locker and close it. The third goes to every third locker and, if it is closed, he opens it, and if it is open, he closes it. The fourth student does this to every fourth locker, and so on. After the process is completed with the "n"th student, how many lockers are open?
+//
+// The task is to write a function which gets any number as an input and returns the number of open lockers after last sudent complets his activity. So input of the function is just one number which shows number of lockers and number of students. For example if there are 1000 lockers and 1000 students in school then input is 1000 and function returns number of open lockers after 1000th student completes his action.
+//
+// The given input is always an integer greater than or equal to zero that is why there is no need for validation.
+
+
+
+//solution attempt 1 : brute force
+/*
+function numOfOpenLockers(n){
+  let lockers = new Array(n).fill(true) //true == open ; false == closed
+  let student = 2
+  while (student <= n) {
+    //loop through lockers and if n % student === 0 flip the locker open or closed
+    lockers.forEach((element, index) => {
+      if (((index+1) % student === 0)) {
+        lockers[index] = !lockers[index]
+      }
+    })
+    //increment student and repeate while loop
+    student++
+  }
+  return lockers.filter(element => element === true).length
+}
+*/
+
+/*
+the brute force solution works but is very expensive and times out with larger inputs
+I can't pretend I understand the math here but I ran this algo on all vals < 100
+the output remains constant between perfect squares and then increments at each perfect square
+so there's a relationship and yep there I see it now. Its the sqrt of n rounded down.
+That is so satisfying.
+*/
+function numOfOpenLockers(n) {
+  return Math.floor(Math.sqrt(n));
+}
