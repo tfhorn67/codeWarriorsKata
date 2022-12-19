3d404a9454bb332f72a0e8f14441cab822090119
Tennyson Horn <thorn67@gmail.com>
2022-12-18 19:30:24 -0600

    I am very tired and this is bad

---
+++ largestElements.js
@@ -0,0 +1,16 @@
+// Write a program that outputs the top n elements from a list.
+//
+// Example:
+//
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
+
+
+//solution
+function largest(n,xs){
+  if (n <= 0) return []
+  let len = xs.length
+  for (let i = 0 ; i < len - n ; i++) {
+    xs.splice(xs.indexOf(Math.min(...xs)), 1)
+  }
+  
